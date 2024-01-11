7d6454819deceb17efb39da72bcea9b7966b31e7
Karnakhall <emil_q21@wp.pl>
2024-01-11 12:17:10 +0100

    ActorWorldOffset and ActorWorldRotation
    Multiplay it by Delta Time for smoothing movement through the different FPS

--- Source/ActionRPG/Private/Items/Item.cpp
+++ Source/ActionRPG/Private/Items/Item.cpp
@@ -49,8 +49,15 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	AddActorWorldOffset(FVector(1.f, 0.f, 0.f));			// Przesuwamy "aktora" o 1 w osi X
+	// Movement rate in units of cm/s
+	float MovementRate = 50.f;
+	float RotationRate = 45.f;
+
+	// MovementRate * DeltaTime (cm/s) * (s/frame) = (cm/frame)
+	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));			// Przesuwamy "aktora" o 50 w osi X, bez wzglêdu na to ile FPS ma dany komputer
+	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
+	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 
 
 	
