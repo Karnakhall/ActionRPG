e3e9d0ad00dcac2530796d1451354396d01bcf68
Karnakhall <emil_q21@wp.pl>
2024-01-22 17:24:45 +0100

    Added skeletalmeshcomponent

--- Source/ActionRPG/Public/Pawns/Bird.h
+++ Source/ActionRPG/Public/Pawns/Bird.h
@@ -7,6 +7,7 @@
 #include "Bird.generated.h"
 
 class UCapsuleComponent;
+class USkeletalMeshComponent;
 
 UCLASS()
 class ACTIONRPG_API ABird : public APawn
@@ -30,5 +31,8 @@ protected:
 private:
 	UPROPERTY(VisibleAnywhere)
 	UCapsuleComponent* Capsule;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* BirdMesh;
 
 };
