b398f53d96780286f16ec84993191a798d2961c9
Karnakhall <emil_q21@wp.pl>
2024-02-09 13:58:28 +0100

    Update SlashCharacter.cpp

--- Source/ActionRPG/Private/Characters/SlashCharacter.cpp
+++ Source/ActionRPG/Private/Characters/SlashCharacter.cpp
@@ -69,10 +69,18 @@ void ASlashCharacter::MoveRight(float Value)
 {
 	if (Controller && (Value != 0.f))
 	{
-		
-		FVector Right = GetActorRightVector();
+		//Moving only in the left and right	direction
+		/*FVector Right = GetActorRightVector();
 		AddMovementInput(Right, Value);
-		
+		*/
+
+		//find out which way is right
+		const FRotator ControlRotation = GetControlRotation();
+		const FRotator YawRotation(0.f, ControlRotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+
 	}
 }
 
