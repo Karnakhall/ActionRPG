c81e5cc5542078b1be3e9cd892fb8fb85f04322f
Karnakhall <emil_q21@wp.pl>
2024-02-19 20:24:52 +0100

    Added AnimInstance

--- Source/ActionRPG/Public/Characters/SlashAnimInstance.h
+++ Source/ActionRPG/Public/Characters/SlashAnimInstance.h
@@ -13,5 +13,17 @@ UCLASS()
 class ACTIONRPG_API USlashAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime)	override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class ASlashCharacter* SlashCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	class UCharacterMovementComponent* SlashCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
 };
