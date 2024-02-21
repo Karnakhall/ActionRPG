7df82ff02257400137bb16c1ed4987119a78596d
Karnakhall <emil_q21@wp.pl>
2024-02-21 15:46:07 +0100

    Added virtual overlap into item

--- Source/ActionRPG/Public/Items/Weapons/Weapon.h
+++ Source/ActionRPG/Public/Items/Weapons/Weapon.h
@@ -13,5 +13,12 @@ UCLASS()
 class ACTIONRPG_API AWeapon : public AItem
 {
 	GENERATED_BODY()
-	
+protected:
+	//Delegate function for sphere overlap
+	//UFUNCTION nie mo¿e byæ w klasie pochodnej. Dostaniemy b³êdy w kompilacji 
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	//Delegate funtion for sphere endoverlap
+	//UFUNCTION nie mo¿e byæ w klasie pochodnej. Dostaniemy b³êdy w kompilacji
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 };
