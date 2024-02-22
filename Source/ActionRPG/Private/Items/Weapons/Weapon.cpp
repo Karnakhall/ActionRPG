7c257658e8d86928e090bb203d16ba88db6ffb43
Karnakhall <emil_q21@wp.pl>
2024-02-22 13:56:20 +0100

    Update Weapon.cpp
    Dodanie możliwości doczepiania broni do postaci

--- Source/ActionRPG/Private/Items/Weapons/Weapon.cpp
+++ Source/ActionRPG/Private/Items/Weapons/Weapon.cpp
@@ -2,10 +2,19 @@
 
 
 #include "Items/Weapons/Weapon.h"
+#include "Characters/SlashCharacter.h"	//Musimy dodaæ plik nag³ówkowy z naszym bohaterem aby móc podnieœæ broñ
 
 void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComponent, OtherBodyIndex, bFromSweep, SweepResult);
+	//Sprawdzamy czy to jest nasz bohater, jeœli tak to podnoœmy broñ i doczepiomy do socketu w d³oni, który stworzylismy
+	ASlashCharacter* SlashCharacter = Cast<ASlashCharacter>(OtherActor);
+	if (SlashCharacter)
+	{
+		//Zasady przyczepienia broni do socketu d³oni
+		FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
+		ItemMesh->AttachToComponent(SlashCharacter->GetMesh(), TransformRules, FName("RightHandSocket"/*Podajemy nazwê socketu do któego ma zostac doczepiona broñ*/));
+	}
 }
 
 void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
