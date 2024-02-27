f3922a0e3105fceac490b209540fc0a2577526e6
Karnakhall <emil_q21@wp.pl>
2024-02-27 13:18:27 +0100

    Update Weapon.cpp
    Po podniesieniu broni zmieniamy status na wyekwipowany

--- Source/ActionRPG/Private/Items/Weapons/Weapon.cpp
+++ Source/ActionRPG/Private/Items/Weapons/Weapon.cpp
@@ -6,9 +6,11 @@
 
 void AWeapon::Equip(USceneComponent* InParent, FName InSocketName)
 {
-	//Tworzymy zasady podniszenia broni
+	//Tworzymy zasady podnoszenia broni
 	FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
 	ItemMesh->AttachToComponent(InParent, TransformRules, InSocketName);
+	//Zmieniamy stan broni na EIS_Equipped po podniesieniu
+	ItemState = EItemState::EIS_Equipped;
 }
 
 void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
