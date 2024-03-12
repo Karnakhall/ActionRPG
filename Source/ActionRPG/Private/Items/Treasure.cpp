50d0639623c08bd3a7294fa8f0703a764816f2de
Karnakhall <emil_q21@wp.pl>
2024-03-12 12:20:04 +0100

    Added Sound at location of pickup gold

--- Source/ActionRPG/Private/Items/Treasure.cpp
+++ Source/ActionRPG/Private/Items/Treasure.cpp
@@ -2,4 +2,24 @@
 
 
 #include "Items/Treasure.h"
+#include "Characters/SlashCharacter.h"
+#include "Kismet/GameplayStatics.h"
 
+void ATreasure::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//Sprawdzamy czy bohater overlappuje z naszym przedmiotem, jesli tak, mo¿emy podnieœæ przedmiot
+	ASlashCharacter* SlashCharacter = Cast<ASlashCharacter>(OtherActor);
+	if (SlashCharacter)
+	{
+		if (PickupSound)	//Sprawdzamy czy nie jest nullpointerem
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				PickupSound, 
+				GetActorLocation()
+			);	//Odtwarzamy dŸwiêk podnoszenia przedmiotu
+		}
+		
+		Destroy();	//Jeœli bohater overlappuje z naszym przedmiotem, to niszczymy go
+	}
+}
--- Source/ActionRPG/Public/Items/Treasure.h
+++ Source/ActionRPG/Public/Items/Treasure.h
@@ -13,5 +13,12 @@ UCLASS()
 class ACTIONRPG_API ATreasure : public AItem
 {
 	GENERATED_BODY()
-	
+protected:
+
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;	//Override the OnSphereOverlap function from the base class
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+	USoundBase* PickupSound;	//DŸwiêk podnoszenia z³ota. Przechowujemy ten asset we wskaŸniku
 };
